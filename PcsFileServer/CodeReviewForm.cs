08221f487fc7395a1013b22f3578b7579fa340a1
Unpoin <vasilyev.roman2013@gmail.com>
2022-11-18 11:57:33 +0300

    Email code recover

---
+++ PcsFileServer/CodeReviewForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+
+namespace PcsFileServer
+{
+    public partial class CodeReviewForm : MetroForm
+    {
+        private string email;
+        public CodeReviewForm(string email)
+        {
+            this.components.SetStyleDark(this);
+            InitializeComponent();
+            this.email = email;
+        }
+
+        private void RecoverButton_Click(object sender, EventArgs e)
+        {
+            int codeFromEmail;
+            RecoverForm.emailCodes.TryGetValue(email, out codeFromEmail);
+            int codeFromForm = Int32.Parse(CodeTextBox.Text);
+            if (codeFromEmail == codeFromForm)
+            {
+                MessageBox.Show("Вы ввели правильный код");
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели неправильный код");
+            }
+        }
+    }
+}
+
--- PcsFileServer/RecoverForm.cs
+++ PcsFileServer/RecoverForm.cs
@@ -16,6 +16,8 @@ namespace PcsFileServer
     public partial class RecoverForm : MetroForm
     {
         //icqbqthqpmhvidqb ya pass
+        public static Dictionary<string, int> emailCodes = new Dictionary<string, int>();
+
 
         public RecoverForm()
         {
@@ -27,16 +29,18 @@ namespace PcsFileServer
         {
             try
             {
+                int emailCode = new Random().Next(1000,9999);
                 // отправитель - устанавливаем адрес и отображаемое в письме имя
                 MailAddress from = new MailAddress("pcsfileserver@yandex.ru", "Pcs_file_server");
                 // кому отправляем
                 MailAddress to = new MailAddress(MailTextBox.Text);
+                emailCodes.Add(MailTextBox.Text,emailCode);
                 // создаем объект сообщения
                 MailMessage m = new MailMessage(from, to);
                 // тема письма
                 m.Subject = "Тест";
                 // текст письма
-                m.Body = "<h2>Ваш код для восстановления пароля:</h2>";
+                m.Body = $"<h2>Ваш код для восстановления пароля: {emailCode}</h2>";
                 // письмо представляет код html
                 m.IsBodyHtml = true;
                 // адрес smtp-сервера и порт, с которого будем отправлять письмо
@@ -47,6 +51,10 @@ namespace PcsFileServer
                 smtp.Credentials = new NetworkCredential("pcsfileserver@yandex.ru", "icqbqthqpmhvidqb");
                 smtp.Send(m);
                 //Console.Read();
+                CodeReviewForm codeReviewForm = new CodeReviewForm(MailTextBox.Text);
+                this.Hide();
+                codeReviewForm.ShowDialog();
+                this.Close();
             }
             catch (Exception ex)
             {
